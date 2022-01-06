b1d8dde0f5152f7b0b76ccc4f6e2f072d70f3328
Daniel Kirk <imdanielkirk@gmail.com>
2022-01-05 21:57:42 -0800

    woo

---
+++ ChartProject/src/core/Chart.svelte
@@ -0,0 +1,24 @@
+<canvas id="canvasID" ></canvas>
+
+<script>
+    import { Chart } from 'chart.js';
+    import { onMount, onUpdate } from 'svelte';
+    export let 
+      config
+    , width
+    , height
+// need way to conditionally render the id of the canvas prior to mounting the chart //
+// https://www.chartjs.org/docs/latest check here for config usage //
+    const ctx = document.getElementById('canvasID');
+// setting configuration for canvas that chart is rendered in //
+    ctx.id = config.type;
+    ctx.width = width;
+    ctx.height = height;
+// chart creation configurations //
+    const createChart = new Chart(ctx, {
+        type: config.type,
+        data: config.data,
+        options: config.options
+    });
+     onMount(createChart);
+</script>
